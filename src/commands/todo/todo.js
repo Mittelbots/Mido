607cb357b1f372ce9e5a58b1a2a74b5a05061cd6
Mittelblut9 <sauer.benedikt@outlook.de>
2022-03-06 14:04:12 +0100

    add interaction to change list & add buttons interaction

--- src/commands/todo/todo.js
+++ src/commands/todo/todo.js
@@ -1,11 +1,17 @@
 const {
+    MessageButton,
     MessageActionRow,
     MessageEmbed,
     MessageSelectMenu
 } = require('discord.js');
-const { Database } = require('../../../bot/db/db');
+const {
+    Database
+} = require('../../../bot/db/db');
 const config = require('../../../utils/assets/json/_config/config.json');
-const { errorhandler } = require('../../../utils/functions/errorhandler/errorhandler');
+
+const {
+    errorhandler
+} = require('../../../utils/functions/errorhandler/errorhandler');
 const {
     getCategory
 } = require('../../../utils/functions/getData/getCategory');
@@ -15,6 +21,12 @@ const {
 const {
     hasPermissions
 } = require('../../../utils/functions/hasPermissions/hasPermissions');
+const {
+    addButtons
+} = require('../../../utils/functions/toDoList/addButtonsToList');
+const {
+    addSelectMenu
+} = require('../../../utils/functions/toDoList/addSelecMenu');
 
 const database = new Database();
 
@@ -36,29 +48,8 @@ module.exports.run = async (bot, message, args) => {
     const add_catId = 'add_cat';
     const select_catId = 'select_cat';
 
-    var selectMenu = new MessageSelectMenu()
-        .setCustomId(select_catId)
-        .setPlaceholder((categories) ? 'Select category' : 'Füge eine neue hinzu.');
-
-        if(categories !== false) {
-            categories.map(cat => {
-                selectMenu.addOptions([{
-                    'value': select_catId + cat.id,
-                    'label': cat.name,
-                    'description': 'Klicke hier um Kategorie auszuwählen',
-                }])
-            })
-        }else {
-            selectMenu.addOptions([{
-                'value': add_catId,
-                'label': 'Füge eine neue Kategorie hinzu.',
-                'description': 'Klicke hier'
-            }])
-        }
-
-
     var newMessageEmbedInteraction = new MessageActionRow()
-        .addComponents(selectMenu)
+        .addComponents(addSelectMenu(categories, select_catId, add_catId))
 
     message.reply({
         embeds: [newMessageEmbed],
@@ -66,16 +57,16 @@ module.exports.run = async (bot, message, args) => {
     });
 
     bot.on('interactionCreate', async (interaction) => {
-        if(interaction.isSelectMenu() && interaction.customId === 'select_cat') {
+        if (interaction.isSelectMenu() && interaction.customId === 'select_cat') {
             await interaction.deferUpdate();
-            if(interaction.values.indexOf(add_catId) !== -1) {
+            if (interaction.values.indexOf(add_catId) !== -1) {
                 await message.channel.send('Bitte gebe einen Namen ein!')
                 var messageCollector = await message.channel.createMessageCollector({
                     filter: (() => message.author.id),
                     time: 15000,
                     max: 1
                 });
-    
+
                 messageCollector.on('collect', async reply => {
                     return await database.query('INSERT INTO hn_category (name, color) VALUES (?, ?)', [reply.content, '#021982'])
                         .then(() => {
@@ -90,29 +81,57 @@ module.exports.run = async (bot, message, args) => {
                         });
                 });
 
+            } else {
+                categories.map(async cat => {
+                    if (interaction.values.indexOf(select_catId + cat.id) !== -1) {
+
+                        newMessageEmbed = null;
+                        newMessageEmbed = new MessageEmbed()
+                        newMessageEmbed.setTitle(`ToDo Liste - ${cat.name}`)
+                        newMessageEmbed.setColor(cat.color)
+
+                        todo.map(todo => {
+                            if (todo.cat_id === cat.id) {
+                                newMessageEmbed.addField('‎\n⏹️ ' + todo.title, '- _' + todo.text + '_ \n ||ID:' + todo.id + '||');
+                            }
+                        })
+
+                        interaction.message.edit({
+                            embeds: []
+                        });
+
+                        interaction.message.edit({
+                            embeds: [newMessageEmbed],
+                            components: [new MessageActionRow({
+                                components: [addButtons()[0], addButtons()[1], addButtons()[2]]
+                            })]
+                        })
+
+                        const collector = await interaction.message.createMessageComponentCollector();
+
+                        collector.on('collect', async interaction => {
+                            switch (interaction.customId) {
+                                case 'add_toDo':
+                                    break;
+
+                                case 'change_cat':
+                                    interaction.message.edit({
+                                        components: [new MessageActionRow({
+                                            components: [addSelectMenu(categories, select_catId, add_catId)]
+                                        })]
+                                    })
+                                    break;
+
+                                case 'delete_toDo':
+                                    break;
+
+                                default:
+                                    break;
+                            }
+                        });
+                    }
+                });
             }
-            categories.map(cat => {
-                if(interaction.values.indexOf(select_catId + cat.id) !== -1) {
-
-                    newMessageEmbed = null;
-                    newMessageEmbed = new MessageEmbed()
-                    newMessageEmbed.setTitle(`ToDo Liste - ${cat.name}`)
-                    newMessageEmbed.setColor(cat.color)
-                    todo.map(todo =>{
-                        if(todo.cat_id === cat.id) {
-                            newMessageEmbed.addField('‎\n⏹️ ' + todo.title, '- _' + todo.text + '_ \n ');
-                        }
-                    })
-
-                    interaction.message.edit({
-                        embeds: []
-                    })
-                    
-                    return interaction.message.edit({
-                        embeds: [newMessageEmbed]
-                    })
-                }
-            });
         }
     });
 }
