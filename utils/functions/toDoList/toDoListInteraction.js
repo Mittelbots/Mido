a886b1b4fe9490240941b512581866f3f93ad497
Mittelblut9 <sauer.benedikt@outlook.de>
2022-03-14 22:50:17 +0100

    modified:   utils/functions/toDoList/toDoListInteraction.js 	modified:   utils/variables/variables.js

--- utils/functions/toDoList/toDoListInteraction.js
+++ utils/functions/toDoList/toDoListInteraction.js
@@ -1,6 +1,6 @@
 const { MessageEmbed, MessageActionRow } = require("discord.js");
 const database = require("../../../bot/db/db");
-const { add_catId, select_catId, categories, todo } = require("../../variables/variables");
+const { add_catId, select_catId } = require("../../variables/variables");
 const { delay } = require("../delay/delay");
 const { errorhandler } = require("../errorhandler/errorhandler");
 const { getCategory } = require("../getData/getCategory");
@@ -14,6 +14,9 @@ const { viewToDoList } = require("./viewToDoList");
 var count = 0;
 
 module.exports.todoListInteraction = async (main_interaction) => {
+
+    var categories;
+    var todo;
 
     async function refreshCategories_ToDo() {
         categories = await getCategory(main_interaction.message.channel);
--- utils/variables/variables.js
+++ utils/variables/variables.js
@@ -1,5 +1,2 @@
 module.exports.add_catId = 'add_cat';
 module.exports.select_catId = 'select_cat';
-
-module.exports.categories = {};
-module.exports.todo = {};
