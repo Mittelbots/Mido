4a35214879dc589a86b2191d5706109d568e02dc
Mittelblut9 <sauer.benedikt@outlook.de>
2022-04-22 13:07:18 +0200

    added slash command handler & changed ping cmd to slash command

--- utils/functions/deployCommands/deployCommands.js
+++ utils/functions/deployCommands/deployCommands.js
@@ -2,22 +2,28 @@ const fs = require('fs');
 const { log } = require('../../../logs');
 const config = require('../../assets/json/_config/config.json');
 
-function deployCommands(bot) {
-        let modules = fs.readdirSync('./src/commands/');
-        modules.forEach((module) => {
-            fs.readdir(`./src/commands/${module}`, (err, files) => {
-                if (err) {
+async function deployCommands({bot}) {
+    let modules = fs.readdirSync('./src/commands/');
+
+    modules.forEach((module) => {
+        fs.readdir(`./src/commands/${module}`, (err, files) => {
+            if (err) {
+                if(err.path === './src/slash_commands/index.js') {return}
+                else {
                     log.warn('Missing folder!', err)
                     if (config.debug) console.log(`Mission Folder!!`, err);
                 }
-                files.forEach((file) => {
-                    if (!file.endsWith('.js')) return;
-                    var command = require(`../../../src/commands/${module}/${file}`);
-                    console.log(`${command.help.name} Command has been loaded!`);
-                    if (command.help.name) bot.commands.set(command.help.name, command)
-                })
-            });
+            }
+            files.forEach((file) => {
+                if (!file.endsWith('.js')) return;
+                var command = require(`../../../src/commands/${module}/${file}`);
+                console.log(`${command.help.name} Command has been loaded!`);
+                if (command.help.name) {
+                    bot.commands.set(command.help.name, command)
+                }
+            })
         });
+    });
     return;
 }
 
